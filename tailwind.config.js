a674b76683dd1594fc9f64727515bfd5ac1d2af6
Apoorv Rathore <0211ite094@niet.co.in>
2025-12-02 21:02:58 +0530

    add research section

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const darkMode = 'class';
+export const content = [
+    "./src/**/*.{html,ts}"
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
